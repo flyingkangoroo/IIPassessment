bb21348f009d85bf46813ea41760d85b57e5ea69
flyingkangoroo <66832478+flyingkangoroo@users.noreply.github.com>
2024-09-12 07:11:51 +0000

    "sidebar"

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,6 @@ import streamlit as st
 st.title("Welcome to the Business Use-Case Viability Assessment")
 st.write("This app allows you to assess various dimensions of a business use-case and see your results visualized in real time.")
 
+st.sidebar("Menu")
 if st.button("Start Assessment"):
     st.write("Navigate to the 'Assessment' page in the sidebar to begin!")
