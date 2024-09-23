84d4650060a1732483de36a267defe4db72f9311
flyingkangoroo <66832478+flyingkangoroo@users.noreply.github.com>
2024-09-23 16:16:39 +0200

    Updated Questions Presence

--- pages/Assessment: Presence.py
+++ pages/Assessment: Presence.py
@@ -18,24 +18,29 @@ if dimension not in st.session_state.responses or isinstance(st.session_state.re
 
 subdimensions = {
     "Immersion": [
-        """We are planning on integrating the "human factor" in your IIP through accurate posutres, gestures and other human factors.""",
-        "We want the stakeholders of our use-case to experience sensory feedback from the partners in our IIP, such as body language, facial expressions, and eye contact.",
-        "We want to integrate the features ‘immediacy’ and ‘low latency’, ‘virtuality’ and ‘virtual identity’ to enhance the immersiveness.",
-        "Creating a high-quality consumer experience through the creation of realistic environmental simulations could enhance our use-case.",
-        "Generating a sense of presence in a virtual environment such that the user can act and behave as if the IIP environment is real, thereby enabling scenarios or experiments otherwise difficult to reproduce in the real world. immersion in IIPs can help our users to focus on actually experiencing the provided content rather than grasping just the interface would benefit our use-case.",
-        "We want to strengthen our use-case awareness, positioning, branding and/ or management through integrating an IIP."
+        "Realism: Our use-case benefits from integrating realistic human factors, such as postures, gestures, and facial expressions, to create a highly immersive experience.",
+        "Multisensory: Our use-case can enhance immersion through multisensory experiences and haptic feedback to create a more realistic interaction.",
+        "Realistic Simulation: Our IIP aims to represent our use-case in a realistic or pseudo-natural manner, closely mirroring real-world scenarios.",
+        "Platform Usability: We want our IIP to offer easy navigation and a seamless user experience that enhances user engagement and motivation.",
+        "Immersive Scenarios: Our use-case benefits from providing immersive and interactive environments that simulate real-world experiences and enable users to behave as they would in physical scenarios."
         ],
     "Sensors": [
-        "Our IIP should enables users to mobilize their eyes, ears, mouth, nose and other body senses through precreated simulations and interactive devices such as head-mounted displays, earphones, handles and data gloves, and achieve their goals through different ways such as voice, movement, eye movements and gestures, thus achieving a nearly natural and “personal experience” interaction.",
-        "We want to allow users to engage in an authentic and immersive way by integrating the sensors for precise visualization of gestures, postures, facial expressions and movements.",
-        "We recreate physical real-world experiences through the integration of haptic feedback technologies.",
-        "We plan on overlaying digital visual stimuli onto the real world to vitalize real-world experiences and create a more engaging experience.",
-        "We make sure to integrate the necessary sensors to minimize the chance of misunderstandings and erroneous judgements through non-verbal communicationcues"
+        "Multisensory: Our use-case benefits from integrating multiple senses through sensors to provide precise and immersive feedback, including sight, sound, and touch.",
+        "Real-Time Interaction: Our IIP enables users to interact with real-time, accurate representations of the use-case, simulating movements, gestures, and spatial information effectively.",
+        "Haptic Feedback: We aim to simulate tactile sensations and force feedback to create a realistic experience for users engaging with the IIP.",
+        "Visual and Audio Fidelity: Our use-case benefits from high-quality visual and auditory stimuli, providing photorealistic environments and accurate soundscapes.",
+        "Sensor Integration: Our IIP incorporates sensors to facilitate natural interactions such as eye contact, gestures, and facial expressions, creating an authentic experience for users."
+        ],
+    "Simulation": [
+        "Real-World Movements: Our IIP enables realistic simulation of real-world movements, providing a natural and immersive experience for users.",
+        "User Engagement: The integration of digital and physical simulations in our IIP adds value to our use-case by enhancing user engagement and creating more dynamic, immersive experiences."
         ],
     "Interactive Environment": [
-        "It benefits our case if the user has as much control about their interactions with other humans or machines as possible.",
-        "Considering the diversity of interactions between different interfaces and contents is beneficial for our use-case.",
-        "It is important that we enable immediate feedback for our use-case."
+        "User Control: Our IIP allows users to have significant control over their interactions with both virtual environments and other users, enhancing engagement.",
+        "Social and Collaborative Interaction: Our use-case benefits from integrating social interactions and collaboration, where users can interact in real-time with others and contribute to a shared virtual space.",
+        "Immersive Feedback: Our use-case benefits from providing real-time feedback to users, enabling immersive interactions and immediate responses in a virtual environment.",
+        "User Experience and Usability: A strong focus on user experience and usability within our IIP is critical for enhancing emotional involvement and user satisfaction.",
+        "Real-World Interaction: Our use-case benefits from replicating real-world interactions within the IIP, where users’ actions in the virtual world have meaningful effects on both virtual and real environments."
         ]
 }
 
